f0ec31316188105033c4ec12bc69f08513174f1a
rflx <rflx@rflx.xyz>
2023-09-30 18:44:55 +0300

    Clean up

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
-from typing import Union
-from fastapi import FastAPI, Request, HTTPException
 import jwt
-from pydantic import BaseModel
-from config import ALGORITHM, KEYCLOACK_SERVER
-
 import requests
-
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-
-# async def request(client):
-#     response = await client.get("https://{KEYCLOCK_SERVER}/auth/realms/master")
-#     return response.text
-
-
-# @app.get('/')
-# async def main():
-#     request = requests.get('https://localhost:8080/realms/master/protocol/openid-connect/certs')
-#     # rsa_public_key = request['access_token']
-
-#     # print(rsa_public_key)
-#     print(request)
 
 
 @app.get("/validate/{jwt_token}")
 async def validate(jwt_token):
     response = requests.get("http://localhost:8080/realms/master/").json()
     public_key = response['public_key']
-    
 
     try:
         decoded_payload = jwt.decode(
             jwt_token,
-            '-----BEGIN PUBLIC KEY-----\n' + public_key + '\n-----END PUBLIC KEY-----', 
+            '-----BEGIN PUBLIC KEY-----\n' + public_key + '\n-----END PUBLIC KEY-----',
             algorithms=["RS256"],
             audience="account"
         )
