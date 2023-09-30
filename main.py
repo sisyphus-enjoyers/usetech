ffb4cb36380154c66b350d0f2bb480f8130d6b7f
rflx <rflx@rflx.xyz>
2023-09-30 22:45:14 +0300

    Use authorization headers instead

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-import jwt
 import requests
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Header
+from jose import jwt
 
 app = FastAPI()
 
 
-@app.get("/validate/{jwt_token}")
-async def validate(jwt_token):
+@app.get("/")
+async def validate(authorization: str = Header(None)):
+    if authorization is None:
+        return {"error": "Authorization header is missing"}
+
+    parts = authorization.split()
+    if len(parts) != 2 or parts[0].lower() != "bearer":
+        return {"error": "Invalid Authorization header format"}
+
+    token = parts[1]
+
     response = requests.get("http://localhost:8080/realms/master/").json()
     public_key = response['public_key']
 
     try:
-        decoded_payload = jwt.decode(
-            jwt_token,
+        jwt.decode(
+            token,
             '-----BEGIN PUBLIC KEY-----\n' + public_key + '\n-----END PUBLIC KEY-----',
             algorithms=["RS256"],
             audience="account"
         )
-        return decoded_payload
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=403, detail="Error")
-
-
-@app.get("/journal/")
-async def journal():
-    return {"Response": "It`s work"}
-
-
-@app.get("/control/")
-async def control():
-    return {"Response": "It`s work"}
+        return
+    except jwt.JWTError:
+        raise HTTPException(status_code=403, detail="Invalid credentials")
